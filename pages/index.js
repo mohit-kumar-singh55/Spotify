b93e520e8e213ebdf1710b7b60353a4729ddf733
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-27 19:46:06 +0530

    creating Sidebar

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,20 @@ import Head from 'next/head';
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Spotify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>This is Spotify</h1>
+      <main>
+        {/* <Sidebar /> */}
+        {/* <Center /> */}
+      </main>
+
+      <div>
+        {/* <Player /> */}
+      </div>
 
     </div>
   )
