29e7c20b16732eb56524cc7f1c4d217a2c105cd5
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-28 19:02:32 +0530

    making login page

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="bg-black overflow-hidden h-screen">
       <Head>
         <title>Spotify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className=''>
         <Sidebar />
         {/* <Center /> */}
       </main>
