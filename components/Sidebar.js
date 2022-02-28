41d21e9c4961daa1d5551e8b0505fa3d6b22590e
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-28 18:51:45 +0530

    created considerable sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { HomeIcon, SearchIcon, LibraryIcon, HeartIcon, RssIcon } from "@heroicons/react/outline";
+import { HomeIcon, SearchIcon, LibraryIcon, HeartIcon, RssIcon, PlusCircleIcon } from "@heroicons/react/outline";
 
 const Sidebar = () => {
     return (
         <div className='text-gray p-5 text-sm border-r border-gray-900'>
-            <div>
+            <div className='space-y-4'>
                 <button className='flex items-center space-x-2 hover:text-white'>
                     <HomeIcon className="h-5 w-5" />
                     <p>Home</p>
@@ -21,21 +21,21 @@ const Sidebar = () => {
                 <hr className='border-t-[0.1px] border-gray-900' />
 
                 <button className='flex items-center space-x-2 hover:text-white'>
+                    <PlusCircleIcon className="h-5 w-5" />
+                    <p>Create Playlist</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
                     <HeartIcon className="h-5 w-5" />
-                    <p>Home</p>
+                    <p>Liked Songs</p>
                 </button>
                 <button className='flex items-center space-x-2 hover:text-white'>
                     <RssIcon className="h-5 w-5" />
-                    <p>Search</p>
-                </button>
-                <button className='flex items-center space-x-2 hover:text-white'>
-                    <LibraryIcon className="h-5 w-5" />
-                    <p>Your Library</p>
+                    <p>Your episodes</p>
                 </button>
 
                 <hr className='border-t-[0.1px] border-gray-900' />
 
-
+                {/* Playlists */}
             </div>
         </div>
     )
