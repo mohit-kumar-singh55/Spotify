c72e00673e9d76c6f27b15a9b60e80955176129d
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-02 19:50:33 +0530

    implementing spotify web api

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const login = () => {
     return (
@@ -6,4 +6,4 @@ const login = () => {
     )
 }
 
-export default login
+export default login;
