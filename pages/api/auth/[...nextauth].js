771979d60e889519f4c6801826f39243f10dd44e
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-03 18:37:41 +0530

    nextauth to be completed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,20 @@
-import NextAuth from "next-auth"
-import SpotifyProvider from "next-auth/providers/spotify"
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+    try {
+
+    }
+    catch (e) {
+        console.log(e);
+
+        return {
+            ...token,
+            error: 'REFRESH ACCESS TOKEN'
+        }
+    }
+}
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -11,4 +26,32 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        async jwt({ token, account, user }) {
+            // Initial SignIn
+            if (account && user) {
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                    accessTokenExpires: account.expires_at * 1000
+                }
+            }
+
+            // Return the token if it is not expired yet
+            if (Date.now() < token.accessTokenExpires) {
+                console.log("EXISTING TOKEN IS VALID");
+                return token;
+            }
+
+            // Access Token has expired, so we need to refresh it...
+            console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
+            return await refreshAccessToken(token);
+        }
+    }
 })
