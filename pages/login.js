37478efa527290aa8c3fe73914c2dda63ab9aeaa
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-04 18:39:57 +0530

    no error

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,19 @@ import { getProviders, signIn } from "next-auth/react";
 
 const login = ({ providers }) => {
     return (
-        <div>login</div>
+        <div className='flex flex-col min-h-screen items-center justify-center bg-black'>
+            <img className='w-52 mb-5' src="https://links.papareact.com/9xl" alt="logo" />
+            {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                    <button
+                        className='bg-[#18D860] text-white p-5 rounded-full'
+                        onClick={() => signIn(provider.id, { callbackUrl: "/" })}   // whenever I will change the website I need to change the Redirect URIs in developer.spotify.com's setting to https://mysite.com/api/auth/callback/spotify
+                    >
+                        Login with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </div>
     )
 }
 
