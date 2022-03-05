bdd46ef24a7a7f8bd21b4c1756a144ff2e9e486a
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-05 19:46:46 +0530

    image is not in the session

--- components/Center.js
+++ components/Center.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSession } from "next-auth/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
 
 const Center = () => {
     const { data: session } = useSession();
 
+    const colors = [
+        "from-purple-500",
+        "from-pink-500",
+        "from-blue-500",
+        "from-green-500",
+        "from-red-500",
+        "from-cyan-500",
+        "from-violet-500",
+        "from-orange-500",
+    ]
+
+    useEffect(() => {
+      
+    }, [])
+    
+
     return (
-        <div className='flex flex-grow text-white'>
-            <header>
-                <div>
-                    <img src={session?.user?.image} alt="user_img" />
-                    {console.log(session)}
+        <div className='flex-grow'>
+            <header className='absolute top-5 right-8'>
+                <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+                    <img className='rounded-full w-10 h-10' src={session?.user?.image} alt="user_img" />
+                    <h2>{session?.user.name}</h2>
+                    <ChevronDownIcon className='w-5 h-5' />
                 </div>
             </header>
+
+            <section className={`flex items-end space-x-7 bg-gradient-to-b from-red-500 to-black h-80 text-white p-8`}>
+
+            </section>
         </div>
     )
 }
