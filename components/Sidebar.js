9ee080262bccd0849d88f348de729940ea589932
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-05 20:49:28 +0530

    getting playlist but token is not access refrehing

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HomeIcon, SearchIcon, LibraryIcon, HeartIcon, RssIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
+import useSpofity from "../hooks/useSpotify";
 
 const Sidebar = () => {
     const { data: session, status } = useSession();
+    const [playList, setPlayList] = useState([])
+
+    const spotifyApi = useSpofity()
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPlayList(data.body.items);
+            })
+        }
+    }, [session, spotifyApi])
+
+    console.log(playList);
+
 
     return (
         <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen'>
@@ -39,13 +54,9 @@ const Sidebar = () => {
                 <hr className='border-t-[0.1px] border-gray-900' />
 
                 {/* Playlists */}
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-                <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+                {playList.map((playList) => (
+                    <p className='cursor-pointer hover:text-white'>{playList.name}</p>
+                ))}
             </div>
         </div>
     )
