70947e0d67eb3fc09b49da88fea88c9a54f86a60
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-07 17:10:59 +0530

    making custom hook

--- components/Player.js
+++ components/Player.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSession } from 'next-auth/react';
+import useSpotify from '../hooks/useSpotify';
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 
 const Player = () => {
+    const spotifyApi = useSpotify();
+    const { data: session } = useSession();
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+    const [volume, setVolume] = useState(50)
+
     return (
-        <div>Player</div>
+        <div>
+            {/* left */}
+            <div>
+                {/* <img src="" alt="" /> */}
+            </div>
+        </div>
     )
 }
 
---
+++ hooks/useSongInfo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useSongInfo = () => {
+    return (
+        <div>useSongInfo</div>
+    )
+}
+
+export default useSongInfo
