c40ce244fa778989adc7debd25895fb5439fb06d
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-07 19:25:00 +0530

    applied debouncing in volume

--- components/Player.js
+++ components/Player.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useSession } from 'next-auth/react';
 import useSpotify from '../hooks/useSpotify';
 import useSongInfo from '../hooks/useSongInfo';
@@ -6,6 +6,7 @@ import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import { HeartIcon, VolumeUpIcon as VolumeDownIcon } from "@heroicons/react/outline";
 import { RewindIcon, SwitchHorizontalIcon, VolumeUpIcon, ReplyIcon, PlayIcon, PauseIcon, FastForwardIcon } from "@heroicons/react/solid";
+import { debounce } from 'lodash';
 
 const Player = () => {
     const spotifyApi = useSpotify();
@@ -43,6 +44,19 @@ const Player = () => {
         })
     }
 
+    const debouncedJustVolume = useCallback(
+        debounce((volume) => {
+            spotifyApi.setVolume(volume).catch((err) => { console.log(err) })
+        }, 500),            // delay, waits for event to stop
+        []
+    )
+
+    useEffect(() => {
+        if (volume > 0 && volume < 100) {
+            debouncedJustVolume(volume);
+        }
+    }, [volume])
+
     return (
         <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
             {/* left */}
@@ -69,8 +83,10 @@ const Player = () => {
             </div>
 
             {/* Right */}
-            <div>
-                
+            <div className='flex items-center space-x-3 justify-end pr-5 md:space-x-4'>
+                <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume - 10)} className='button' />
+                <input type="range" value={volume} onChange={(e) => setVolume(Number(e.target.value))} min={0} max={100} className='w-14 md:w-28' />
+                <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className='button' />
             </div>
         </div>
     )
