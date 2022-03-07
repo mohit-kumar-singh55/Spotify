09a6e0cde0c5b5265d9dfb963803a41a60767031
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-07 18:56:03 +0530

    creating right side

--- components/Player.js
+++ components/Player.js
@@ -4,7 +4,8 @@ import useSpotify from '../hooks/useSpotify';
 import useSongInfo from '../hooks/useSongInfo';
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
-import { SwitchHorizontalIcon } from "@heroicons/react/outline";
+import { HeartIcon, VolumeUpIcon as VolumeDownIcon } from "@heroicons/react/outline";
+import { RewindIcon, SwitchHorizontalIcon, VolumeUpIcon, ReplyIcon, PlayIcon, PauseIcon, FastForwardIcon } from "@heroicons/react/solid";
 
 const Player = () => {
     const spotifyApi = useSpotify();
@@ -29,10 +30,23 @@ const Player = () => {
         }
     }, [currentTrackId, spotifyApi, session])
 
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+            if (data.body.is_playing) {
+                setIsPlaying(false);
+                spotifyApi.pause();
+            }
+            else {
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        })
+    }
+
     return (
         <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
             {/* left */}
-            {songInfo && <div className='flex items-center space-x-4'>
+            {<div className='flex items-center space-x-4'>
                 <img className='hidden md:inline h-10 w-10' src={songInfo?.album?.images?.[0]?.url} alt="album" />
                 <div>
                     <h3>{songInfo?.name}</h3>
@@ -41,8 +55,22 @@ const Player = () => {
             </div>}
 
             {/* center */}
-            <div>
+            <div className='flex items-center justify-evenly'>
                 <SwitchHorizontalIcon className='button' />
+                <RewindIcon className='button' />
+                {isPlaying ? (
+                    < PauseIcon onClick={handlePlayPause} className='button w-10 h-10' />
+                ) : (
+                    <PlayIcon onClick={handlePlayPause} className='button w-10 h-10' />
+                )}
+
+                <FastForwardIcon className='button' />
+                <ReplyIcon className='button' />
+            </div>
+
+            {/* Right */}
+            <div>
+                
             </div>
         </div>
     )
