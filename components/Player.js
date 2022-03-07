135d6ae06791bb0f647277e5eaf2962b0dcb9017
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-07 18:17:29 +0530

    left side of player component

--- components/Player.js
+++ components/Player.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react';
 import useSpotify from '../hooks/useSpotify';
 import useSongInfo from '../hooks/useSongInfo';
@@ -14,12 +14,30 @@ const Player = () => {
 
     const songInfo = useSongInfo();
 
+    const fetchCurrentSong = () => {
+        if (!songInfo) {
+            spotifyApi.getMyCurrentPlayingTrack().then(data => setCurrentTrackId(data?.body?.item?.id));
+            spotifyApi.getMyCurrentPlaybackState().then(data => setIsPlaying(data?.body?.is_playing));
+        }
+    }
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken() && !currentTrackId) {
+            fetchCurrentSong();
+            setVolume(50);
+        }
+    }, [currentTrackId, spotifyApi, session])
+
     return (
-        <div>
+        <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
             {/* left */}
-            <div>
+            {songInfo && <div className='flex items-center space-x-4'>
                 <img className='hidden md:inline h-10 w-10' src={songInfo?.album?.images?.[0]?.url} alt="album" />
-            </div>
+                <div>
+                    <h3>{songInfo?.name}</h3>
+                    <p>{songInfo?.artists?.[0].name}</p>
+                </div>
+            </div>}
         </div>
     )
 }
