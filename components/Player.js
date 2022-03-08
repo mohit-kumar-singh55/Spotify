10d34120c2c363bd8b4575d335419294667735e0
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-08 16:09:55 +0530

    cleaning up

--- components/Player.js
+++ components/Player.js
@@ -60,7 +60,7 @@ const Player = () => {
     return (
         <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
             {/* left */}
-            {<div className='flex items-center space-x-4'>
+            {isPlaying && <div className='flex items-center space-x-4'>
                 <img className='hidden md:inline h-10 w-10' src={songInfo?.album?.images?.[0]?.url} alt="album" />
                 <div>
                     <h3>{songInfo?.name}</h3>
@@ -69,7 +69,7 @@ const Player = () => {
             </div>}
 
             {/* center */}
-            <div className='flex items-center justify-evenly'>
+            {isPlaying && <div className='flex items-center justify-evenly'>
                 <SwitchHorizontalIcon className='button' />
                 <RewindIcon className='button' />
                 {isPlaying ? (
@@ -80,14 +80,14 @@ const Player = () => {
 
                 <FastForwardIcon className='button' />
                 <ReplyIcon className='button' />
-            </div>
+            </div>}
 
             {/* Right */}
-            <div className='flex items-center space-x-3 justify-end pr-5 md:space-x-4'>
+            {isPlaying && <div className='flex items-center space-x-3 justify-end pr-5 md:space-x-4'>
                 <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume - 10)} className='button' />
                 <input type="range" value={volume} onChange={(e) => setVolume(Number(e.target.value))} min={0} max={100} className='w-14 md:w-28' />
                 <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className='button' />
-            </div>
+            </div>}
         </div>
     )
 }
