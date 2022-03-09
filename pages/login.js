511c9f8fddc327183b2b309cc45e88c371a0df08
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-09 20:51:34 +0530

    Title -> new

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { getProviders, signIn } from "next-auth/react";
 
+
 const login = ({ providers }) => {
+    useEffect(() => {
+        document.title = "Login | Spotify"
+    }, [])
+
     return (
         <div className='flex flex-col min-h-screen items-center justify-center bg-black'>
             <img className='w-52 mb-5' src="https://links.papareact.com/9xl" alt="logo" />
