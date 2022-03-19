d914ae9e82cc3103116caf8c2f958319a2adf6c9
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-19 19:10:18 +0530

    modal completed

--- components/Modal.js
+++ components/Modal.js
@@ -3,9 +3,13 @@ import { useRecoilState } from "recoil";
 import { Dialog, Transition } from "@headlessui/react";
 import { showModalState } from "../atoms/modalAtom";
 import { SearchIcon } from "@heroicons/react/outline";
+import { useRecoilValue } from 'recoil';
+import { playlistState } from '../atoms/playlistAtom';
+import Song from './Song';
 
 const Modal = () => {
     const [open, setOpen] = useRecoilState(showModalState);
+    const playlist = useRecoilValue(playlistState);
 
     return (
         <Transition.Root show={open} as={Fragment}>
@@ -40,14 +44,18 @@ const Modal = () => {
                         leaveFrom='opacity-100 translate-y-0 sm:scale-95'
                         leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
                     >
-                        <div className='inline-block align-bottom bg-[#282828] text-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl h-fit transform transition-all lg:max-w-5xl md:max-w-3xl sm:my-8 sm:align-middle sm:w-full sm:p-6 sm:max-w-2xl'>
+                        <div className='inline-block overflow-y-scroll align-middle bg-[#282828] text-white rounded-xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all h-fit max-h-[650px] lg:max-w-5xl md:max-w-3xl sm:my-8 sm:align-middle sm:w-full sm:p-6 sm:max-w-2xl'>
                             <div>
                                 <div className='relative flex items-center'>
                                     <SearchIcon className='absolute w-5 h-5 text-gray-400 left-3' />
                                     <input type="search" placeholder='Search for music...' className='w-full bg-transparent rounded-full border-gray-400 border px-8 pl-10 py-3 focus:ring-gray-50 transition-all duration-200' />
                                 </div>
 
-                                
+                                <div className='text-white px-8 flex flex-col mt-5 space-y-1 pb-28'>
+                                    {playlist?.tracks?.items.map((track, i) => (
+                                        <Song key={track.track.id} order={i + 1} track={track} />
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </Transition.Child>
--- components/Songs.js
+++ components/Songs.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useRecoilValue } from 'recoil'
-import { playlistState } from '../atoms/playlistAtom'
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { playlistState } from '../atoms/playlistAtom';
 import Song from './Song';
 
 const Songs = () => {
